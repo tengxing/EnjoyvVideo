88f0fe2608e3931888f630d7fef20681ba85f373
Anxiaolu <1466022881@qq.com>
2017-10-15 19:45:02 +0800

    前台页面以及依赖文件完成

---
+++ html/js/search-control.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+	$('.container').css("display","none");
+});
+$('#page_change').bind('click',function(event){
+	alert();
+	$('.container0').css("display","none");
+	$('.container').css("display","block");
+});*/
